d903ef898345b6c4a1b7d59296a5d18df90ed450
jmmoon96@gmail.com <jmmoon96@gmail.com>
2021-10-18 11:34:05 -0400

    Odd way of doing things
    Reading things in through a helper method spreadsheet to
    
    create all the bonuses so that they can then be added to a
    
    Map for weapons and armor

--- src/BonusStats.java
+++ src/BonusStats.java
@@ -4,5 +4,17 @@ public class BonusStats {
 	private String wepType;
 	private String bonusText;
 	
-	private int bonusStatAdd;
+	private int bonusHP;
+	private int bonusStr;
+	private int bonusDef;
+	private int bonusMag;
+	private int bonusMagDef;
+	private int bonusLuck;
+	private int bonusMeleeAtk;
+	private int bonusMagAtk;
+	private int bonusMeleeDef;
+	private int bonusArmMagDef;
+	private int bonusGoldSellPrice;
+	
+	
 }
