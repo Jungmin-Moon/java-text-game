ca57287bb9f4e4d7c1845f3c264133b506d9ac9d
Jungmin Moon <jmmoon96@gmail.com>
2022-07-07 00:00:11 -0400

    Delete src directory

--- src/main_game/Game_Main.java
+++
@@ -1,7 +0,0 @@
-package main_game;
-
-public class Game_Main {
-	public static void main(String[] args) {
-		//System.out.println("Testing");
-	}
-}
--- src/main_game/Job.java
+++
@@ -1,9 +0,0 @@
-package main_game;
-
-public interface Job {
-	public void addStats(Player player);
-	
-	public void levelUpStatAdd(Player player);
-	
-	
-}
