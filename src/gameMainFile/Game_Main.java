469e845a454b9ae6763069e1e887bbb9da3fb37e
Jungmin Moon <jmmoon96@gmail.com>
2022-07-07 00:39:50 -0400

    Added a main file.
    Added main file to be able to test things while also making sure other aspects of the project were okay.

---
+++ src/gameMainFile/Game_Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package gameMainFile;
+
+/**
+ * @author luckyseven67
+ *
+ */
+public class Game_Main {
+
+	//The main file where the game will launch and test
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
