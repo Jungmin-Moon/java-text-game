a1dccd5ef09174f450093f1cf3d853769bf28314
Jungmin Moon <jmmoon96@gmail.com>
2022-07-21 21:19:36 -0400

    Default values
    Added default values to player and constructor.

--- src/playerInfo/Player.java
+++ src/playerInfo/Player.java
@@ -1,5 +1,18 @@
 package playerInfo;
 
 public class Player {
+	String playerName;
+	int baseStrength = 5;
+	int baseMagic = 5;
+	int baseLuck = 5;
+	int baseDef = 5;
+	int baseHP = 20;
+	int baseAgility = 5;
+	
+	
+	Player(String name) {
+		playerName = name;
+	}
+	
 	
 }
