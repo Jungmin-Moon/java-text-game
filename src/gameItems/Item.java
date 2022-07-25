5a19e5a828cbc1f31bb89c8a8d5bde32fd6873d8
Jungmin Moon <jmmoon96@gmail.com>
2022-07-25 05:02:00 -0400

    Added simple methods
    Added simple constructor for item and getters and setters for Item class

--- src/gameItems/Item.java
+++ src/gameItems/Item.java
@@ -6,5 +6,32 @@ package gameItems;
  * consumables will also need to extend and branch off into their own trees with their own properties as well
  */
 public class Item {
-
+	//Every item will always have some sort of gold value and a name
+	//all subsequent items will have their own properties unique to them like consumables and armors and weapons
+	String name;
+	int goldValue;
+	
+	Item (String name, int goldValue) {
+		this.name = name;
+		this.goldValue = goldValue;
+	}
+	
+	//getters and setters 
+	public String getItemName() {
+		return name;
+	}
+	
+	public int getGoldValue() {
+		return goldValue;
+	}
+	
+	
+	//setters
+	public void setNewItemName(String newItemName) {
+		name = newItemName;
+	}
+	
+	public void setNewGoldValue(int newGoldValue) {
+		goldValue = newGoldValue;
+	}
 }
